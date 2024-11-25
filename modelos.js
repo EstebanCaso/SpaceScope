509f5347c9f07d179a609d53ee70c8b5406e63b0
EstebanCaso <eecaso04@gmail.com>
2024-11-24 20:23:47 -0800

    SpaceScope

--- modelos.js
+++ modelos.js
@@ -1,6 +1,6 @@
 
 
-import * as THREE from './node_modules/three';
+import * as THREE from 'three';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
 // Función para cargar modelos y añadirlos al OutlinePass
